82a5fd72d814cb78350f972d9187177e3a3a3a3d
1M0reBug <jordan.quagliatini@gmail.com>
2015-06-01 11:23:11 +0200

    Adding the surrounding anchors function on client side

---
+++ server1.js
@@ -0,0 +1,41 @@
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+
+var io = require('socket.io')(server);
+var redis = require('socket.io-redis');
+io.adapter(redis({host: 'localhost', port:6379}));
+
+
+
+var port = process.env.PORT || 8001;
+var ent = require('ent');
+var path = require('path');
+
+app.set('view engine', 'jade');
+app.set('views', __dirname + '/views');
+app.use(express.static('static'));
+app.locals.pretty = true;
+
+app.get('/', function(req, res) {
+    res.render('index', {port: port});
+});
+
+io.sockets.on('connection', function (socket) {
+  // handle new users
+  socket.on('new_user', function(nick) {
+    nick = ent.encode(nick);
+    socket.nick = nick;
+    socket.broadcast.emit('new_user', nick);
+  });
+
+  // forward message
+  socket.on('message', function (message) {
+    message = ent.encode(message);
+    socket.broadcast.emit('message', {nick: socket.nick, message: message});
+  });
+});
+
+server.listen(port);
+console.log("Express server started on " + port);
+
